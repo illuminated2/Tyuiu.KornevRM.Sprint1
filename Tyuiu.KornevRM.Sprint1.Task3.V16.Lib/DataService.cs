c65daf5f28192740fd0919db4e7475bcaa89ab1b
illuminated2 <illuminated_89@mail.ru>
2024-09-27 23:54:25 +0500

    Task3

---
+++ Tyuiu.KornevRM.Sprint1.Task3.V16.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KornevRM.Sprint1.Task3.V16.Lib
+{
+    public class DataService : ISprint1Task3V16
+    {
+
+        public double CoeffOfQuadraticEquation(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ Tyuiu.KornevRM.Sprint1.Task3.V16/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.KornevRM.Sprint1.Task3.V16.Lib;
+
+namespace Tyuiu.KornevRM.Sprint1.Task3.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***********************************************************************");
+            Console.WriteLine("*ИСКХОДНЫЕ ДАННЫЕ:                                                    *");
+            Console.WriteLine("***********************************************************************");
+
+            
+           
+            Console.Write("Введите первый корень (x1): ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите второй корень (x2): ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+
+           
+            double b = -(x1 + x2);
+
+            
+            Console.WriteLine($"Коэффициент b = {b:F3}");
+            Console.ReadKey();
+
+        }
+    }
+}
