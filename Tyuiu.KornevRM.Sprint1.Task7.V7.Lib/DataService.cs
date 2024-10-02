035f787d4e0c154804c339194b44062a1dcb19b8
illuminated2 <illuminated_89@mail.ru>
2024-10-02 21:56:09 +0500

    Task7

---
+++ Tyuiu.KornevRM.Sprint1.Task7.V7.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KornevRM.Sprint1.Task7.V7.Lib
+{
+    public class DataService : ISprint1Task7V7
+    {
+        public double Calculate(double x, double y)
+        {
+            double res = Math.Round(((Math.Cos(x) / Math.PI - 2 *Math.Pow(y,x)) + 16 * x * Math.Cos(x) * Math.Cos(y) - 2),3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KornevRM.Sprint1.Task7.V7/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KornevRM.Sprint1.Task7.V7.Lib;
+namespace Tyuiu.KornevRM.Sprint1.Task7.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: КОРНЕВ Р. М. | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Корнев Руслан Максимович | ИСПб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение.         *");
+            Console.WriteLine("* по исходным значениям данных, вводимых пользователем.                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*         cosx                                                            *");
+            Console.WriteLine("* z = ------------- + 16cos(xy) - 2                                       *");
+            Console.WriteLine("*       П - 2y^x                                                          *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+            Console.WriteLine("Введите X: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите Y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+
+        }
+    }
+}
