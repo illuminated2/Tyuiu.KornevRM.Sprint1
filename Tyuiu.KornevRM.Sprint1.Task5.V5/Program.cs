5da6c3d8391a6ada9c1b69955c90894118dad7d8
illuminated2 <illuminated_89@mail.ru>
2024-12-01 15:35:13 +0500

    Task5

--- Tyuiu.KornevRM.Sprint1.Task5.V5/Program.cs
+++ Tyuiu.KornevRM.Sprint1.Task5.V5/Program.cs
@@ -7,9 +7,21 @@ namespace Tyuiu.KornevRM.Sprint1.Task5.V5
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-            Console.WriteLine("***********************************************************************");
-            Console.WriteLine("*ИСКХОДНЫЕ ДАННЫЕ:                                                    *");
-            Console.WriteLine("***********************************************************************");
+            Console.Title = "Спринт #1 | Выполнил: Корнев Р.М.  | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Корнев Р.М.  | ИСПб-24-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Присвоить целой переменной d первую цифру из дробной части положительного");
+            Console.WriteLine("* числа x(так, если x = 32.597, то d = 5)                                 *");
+            Console.WriteLine("* с помощью класса Convert.                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
 
 
             Console.WriteLine("Введите число X: ");
